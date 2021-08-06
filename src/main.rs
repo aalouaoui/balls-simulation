901c4ae061815fa040a9a3bbcf89f0931bdfba14
aalouaoui <khalidaalouaoui@gmail.com>
2021-08-06 21:18:37 +0100

    Prevent infinite loop on balls creation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use macroquad::{hash, prelude::*, ui::root_ui};
+use macroquad::prelude::*;
 
 mod ball;
 use ball::Ball;
@@ -18,28 +18,27 @@ async fn main() {
     let mut sw = screen_width();
     let mut sh = screen_height();
     let mut last_render_time = get_time();
+
     loop {
         let dt = (get_time() - last_render_time) as f32;
         last_render_time = get_time();
 
         if screen_width().ne(&sw) || screen_height().ne(&sh) || balls.len() == 0 {
             balls.clear();
-            for _ in 0..10 {
+            for _ in 0..20 {
+                let mut counter = 0;
                 balls.push(loop {
                     let new_ball = Ball::new_random();
                     if !balls.iter().any(|ball| ball.collide_with(&new_ball)) {
                         break new_ball;
                     }
+                    counter += 1;
+                    assert!(counter < 100);
                 });
             }
             sw = screen_width();
             sh = screen_height();
         }
-
-        root_ui().window(hash!(), Vec2::ZERO, vec2(150.0, 100.0), |ui| {
-            ui.label(None, &format!("FPS: {}", get_fps()));
-            ui.label(None, &format!("dt: {}", dt));
-        });
 
         balls.iter_mut().for_each(|ball| ball.update(dt));
         Ball::handle_balls_collision(&mut balls);
